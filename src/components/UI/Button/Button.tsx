28edaca9f595474321bfc61145ab9fed37b15670
Noé <eon.sberk@gmail.com>
2022-07-06 23:22:58 +0200

    fixed wrong styles for button

--- src/components/UI/Button/Button.tsx
+++ src/components/UI/Button/Button.tsx
@@ -19,7 +19,9 @@ export default function Button({
   const styles = `px-6 py-1 w-fit bg-gradient-to-t from-indigo-500 to-indigo-300 text-white font-serif rounded-lg text-lg font-bold cursor-pointer active:translate-y-0.5 transition-all ${className}`;
   return href ? (
     <Link href={href}>
-      <a className={styles}>{children}</a>
+      <a>
+        <button className={styles}>{children}</button>
+      </a>
     </Link>
   ) : (
     <button className={styles} onClick={onClick} {...props}>
