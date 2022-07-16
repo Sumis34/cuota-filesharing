770c3fcd76b2e81863a2812ae8ea192dc1b25884
noe <eon.sberk@gmail.com>
2022-07-16 15:37:48 +0200

    basic copy button

--- src/components/SharePanel/SharePanel.tsx
+++ src/components/SharePanel/SharePanel.tsx
@@ -1,9 +1,13 @@
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Button from "../UI/Button";
+import IconButton from "../UI/Button/IconButton";
+import { FiCopy } from "react-icons/fi";
 import {
   stepAnimationTransition,
   stepAnimationVariants,
 } from "../Uploader/Uploader";
+import { useEffect, useState } from "react";
+import useTimeoutToggle from "../../hooks/useTimeoutToggle";
 
 interface SharePanelProps {
   url: string;
@@ -13,6 +17,12 @@ interface SharePanelProps {
 const handleFocus = (event: any) => event.target.select();
 
 export default function SharePanel({ url, setStep }: SharePanelProps) {
+  const [copied, setCopied] = useTimeoutToggle({ ms: 2000 });
+  const copyToClipboard = async (value: string) => {
+    await navigator.clipboard.writeText(value);
+    setCopied(true);
+  };
+
   return (
     <motion.div
       key="upload-share"
@@ -23,13 +33,37 @@ export default function SharePanel({ url, setStep }: SharePanelProps) {
       transition={stepAnimationTransition}
     >
       <h2>Configure Link</h2>
-      <input
-        value={url}
-        className="select-all"
-        type="text"
-        readOnly
-        onFocus={handleFocus}
-      />
+      <div className="flex gap-3">
+        <input
+          value={url}
+          className="select-all"
+          type="text"
+          readOnly
+          onFocus={handleFocus}
+        />
+        <div className="relative">
+          <AnimatePresence>
+            {copied && (
+              <motion.span
+                initial={{ opacity: 0, y: -40 }}
+                animate={{ opacity: 1, y: -40 }}
+                exit={{ opacity: 0, y: -60 }}
+                style={{ x: "-10%" }}
+                transition={{ duration: 0.2 }}
+                className="bg-green-200 text-green-800 px-2 text-xs absolute rounded-md z-40"
+              >
+                Copied 🥳
+              </motion.span>
+            )}
+          </AnimatePresence>
+          <IconButton
+            className="aspect-square px-3 group"
+            onClick={() => copyToClipboard(url)}
+          >
+            <FiCopy className="text-xl group-hover:text-indigo-500 transition-all" />
+          </IconButton>
+        </div>
+      </div>
       <Button onClick={() => setStep(0)}>Share more</Button>
     </motion.div>
   );
