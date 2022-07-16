871cd34b939f7c316c02bad1fca70f6a13c57409
noe <eon.sberk@gmail.com>
2022-07-16 14:35:12 +0200

    Fixed time format issue and added icons

--- src/components/UploadLoadingPanel/UploadLoadingPanel.tsx
+++ src/components/UploadLoadingPanel/UploadLoadingPanel.tsx
@@ -1,7 +1,7 @@
 import { addSeconds, format } from "date-fns";
 import { motion } from "framer-motion";
 import { useEffect, useState } from "react";
-import { HiX } from "react-icons/hi";
+import { HiArrowSmUp, HiOutlineClock, HiX } from "react-icons/hi";
 import Wave from "react-wavify";
 import IconButton from "../UI/Button/IconButton";
 import {
@@ -27,7 +27,12 @@ export default function UploadLoadingPanel({
   const [averageSpeed, setAverageSpeed] = useState(0);
   const uploadedBytes = Math.round(totalBytes * (progress / 100));
 
-  const duration = format(addSeconds(new Date(0), secondsLeft || 0), "mm:ss");
+  const timeWhenFinished = addSeconds(
+    new Date(0),
+    isFinite(secondsLeft) ? secondsLeft : 0
+  );
+
+  const duration = format(timeWhenFinished, "mm:ss");
 
   useEffect(() => {
     const interval = setInterval(() => onInterval(), 1000);
@@ -59,7 +64,7 @@ export default function UploadLoadingPanel({
 
   return (
     <motion.div
-      key="upload-loading-panel-2"
+      key="upload-loading-panel"
       initial="initial"
       animate="animate"
       exit="exit"
@@ -109,13 +114,15 @@ export default function UploadLoadingPanel({
           </Wave>
         </div>
         <div className="flex gap-2">
-          <span className="bg-green-200 px-2 rounded-md text-sm text-green-800">
+          <span title="Upload speed" className="bg-green-200 pr-2 pl-1 rounded-md text-sm text-green-800 inline-flex items-center">
+            <HiArrowSmUp className="fill-green-800" />
             {bytes(averageSpeed)}/s
           </span>
           <span
-            title={`Upload finished in ${duration}`}
-            className="bg-indigo-200 px-2 rounded-md text-sm text-indigo-800"
+            title="Upload duration"
+            className="bg-indigo-200 px-2 rounded-md text-sm text-indigo-800 inline-flex items-center gap-1"
           >
+            <HiOutlineClock className="stroke-indigo-800" />
             {duration}
           </span>
         </div>
