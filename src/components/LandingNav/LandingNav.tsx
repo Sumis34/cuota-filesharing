bca7d50451a493066e06832e650b310114859aa6
noe <eon.sberk@gmail.com>
2022-07-17 19:28:41 +0200

    Optemised mobile view

--- src/components/LandingNav/LandingNav.tsx
+++ src/components/LandingNav/LandingNav.tsx
@@ -4,7 +4,7 @@ import Badge from "../UI/Badge";
 export default function LandingNav() {
   return (
     <div className="left-0 top-0 fixed z-50 flex w-screen justify-center">
-      <nav className="px-20 py-12 w-full max-w-screen-2xl">
+      <nav className="sm:px-20 px-5 py-12 w-full max-w-screen-2xl">
         <Link href="/">
           <a className="flex items-center gap-2">
             <h2>cuota.</h2>
