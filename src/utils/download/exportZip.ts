d8794874f1817504ea405a1d8b26932db4c0743f
noe <eon.sberk@gmail.com>
2022-07-27 21:20:04 +0200

    new zip name

--- src/utils/download/exportZip.ts
+++ src/utils/download/exportZip.ts
@@ -1,14 +1,17 @@
 import JsZip from "jszip";
 import FileSaver from "file-saver";
+import { format } from "date-fns";
 
-export const exportZip = (files: File[]) => {
+export const exportZip = async (files: File[]) => {
   const zip = JsZip();
   files.forEach((file) => {
     zip.file(file.name, file);
   });
-  zip.generateAsync({ type: "blob" }).then((zipFile: any) => {
-    const currentDate = new Date().getTime();
-    const fileName = `combined-${currentDate}.zip`;
-    return FileSaver.saveAs(zipFile, fileName);
-  });
+  const zipFile = await zip.generateAsync({ type: "blob" });
+
+  const now = new Date();
+  const prettyData = format(now, "yyyy-MM-dd");
+  const fileName = `cuota-files_${prettyData}.zip`;
+
+  return FileSaver.saveAs(zipFile, fileName);
 };
