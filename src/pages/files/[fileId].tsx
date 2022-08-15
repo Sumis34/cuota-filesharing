e0e67d2dcb1f644645de13d88b2addec1d5e37ed
Noé <eon.sberk@gmail.com>
2022-08-15 21:48:32 +0200

    proper loading animation

--- src/pages/files/[fileId].tsx
+++ src/pages/files/[fileId].tsx
@@ -4,7 +4,7 @@ import { getDefaultLayout } from "../../components/Layout/DefaultLayout";
 import Button from "../../components/UI/Button";
 import { useQuery } from "../../utils/trpc";
 import { NextPageWithLayout } from "../_app";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import downloadZip, {
   DownloadProgressEvent,
   RemoteFile,
@@ -72,6 +72,7 @@ const Files: NextPageWithLayout = () => {
     setUrl(`${window.location.origin}/files/${query.fileId}`);
   }, [query]);
 
+  //TODO: Add skeleton loading animation
   return (
     <>
       <DownloadToast
@@ -89,54 +90,71 @@ const Files: NextPageWithLayout = () => {
       />
       <div className="my-32 relative">
         <main className="relative z-10 pt-32">
-          {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
-          {data && (
-            <PoolStats
-              totalSize={data.totalSize}
-              files={data.files}
-              message={data.message}
-              createdAt={data.poolCreatedAt}
-              expiresAt={data.expiresAt}
-            />
-          )}
-          {query.controls !== "false" && (
-            <motion.div
-              className="flex justify-between mb-3 sticky top-0"
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-            >
-              <PreviewModeButton />
-              <div className="flex gap-3">
-                <QRPopover url={url}>
-                  <IconButton
-                    as="div"
-                    className="h-full aspect-square px-2 group hidden sm:flex items-center justify-center"
+          <AnimatePresence exitBeforeEnter>
+            {isLoading ? (
+              <motion.div
+                key={"loading"}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 0.5 }}
+                className="w-full h-full flex justify-center items-center py-48"
+              >
+                <Ring color="#dddddd" />
+              </motion.div>
+            ) : (
+              <motion.div
+                key={"content"}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ duration: 0.5 }}
+              >
+                {data && (
+                  <PoolStats
+                    totalSize={data.totalSize}
+                    files={data.files}
+                    message={data.message}
+                    createdAt={data.poolCreatedAt}
+                    expiresAt={data.expiresAt}
+                  />
+                )}
+                {query.controls !== "false" && (
+                  <motion.div
+                    className="flex justify-between mb-3 sticky top-0"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
                   >
-                    <HiQrcode className="text-2xl group-hover:text-indigo-500 transition-all text-indigo-800" />
-                  </IconButton>
-                </QRPopover>
-                <Button
-                  title="Download all files as zip"
-                  className="flex items-center gap-2 !px-3"
-                  onClick={() => handleDownloadAll()}
-                >
-                  <HiDownload />
-                </Button>
-              </div>
-            </motion.div>
-          )}
-          {isLoading ? (
-            <Ring color="#dddddd" />
-          ) : (
-            display && display[(query.mode as PreviewMode) || "grid"]
-          )}
+                    <PreviewModeButton />
+                    <div className="flex gap-3">
+                      <QRPopover url={url}>
+                        <IconButton
+                          as="div"
+                          className="h-full aspect-square px-2 group hidden sm:flex items-center justify-center"
+                        >
+                          <HiQrcode className="text-2xl group-hover:text-indigo-500 transition-all text-indigo-800" />
+                        </IconButton>
+                      </QRPopover>
+                      <Button
+                        title="Download all files as zip"
+                        className="flex items-center gap-2 !px-3"
+                        onClick={() => handleDownloadAll()}
+                      >
+                        <HiDownload />
+                      </Button>
+                    </div>
+                  </motion.div>
+                )}
+                {display && display[(query.mode as PreviewMode) || "grid"]}
+              </motion.div>
+            )}
+          </AnimatePresence>
         </main>
         {/* <img
           src="/assets/images/mesh-gradient.png"
           className="absolute top-[-1900px] md:right-[50%] w-full h-full blur-3xl rotate-90 opacity-70"
           alt=""
-        />
-        <img
+          />
+          <img
           src="/assets/images/mesh-gradient.png"
           className="absolute top-[20%] md:left-[70%] blur-2xl opacity-70 animate-pulse"
           alt=""
