c9a82388ba88c7768d98b8cca23cd0235af26e21
Noé <eon.sberk@gmail.com>
2022-11-22 21:32:30 +0100

    half decent login

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -18,23 +18,34 @@ const Login: NextPageWithLayout<{ providers: Provider[] }> = ({
 
   return (
     <div className="my-32 relative">
-      <main className="relative z-10 pt-32">
-        {Object.values(providers).map((provider) =>
-          provider.name === "Google" ? (
-            <GoogleButton
-              onClick={() => signIn(provider.id)}
-              key={provider.name}
-            />
-          ) : (
-            <div key={provider.name}>
-              <button onClick={() => signIn(provider.id)}>
-                Sign in with {provider.name}
-              </button>
-            </div>
-          )
-        )}
-        <Button onClick={() => signOut()}>UnOk</Button>
-        {JSON.stringify(data)}
+      <main className="relative z-10 pt-32 flex justify-center items-center">
+        <div className="text-center w-80 flex-col flex items-center p-10">
+          <h2>Sign in to Cuota</h2>
+          <p className="text-sm opacity-70 mt-1">
+            Sing up or login to Cuota with the following providers
+          </p>
+          <div className="mt-4">
+            {Object.values(providers).map((provider) =>
+              provider.name === "Google" ? (
+                <GoogleButton
+                  onClick={() => signIn(provider.id)}
+                  key={provider.name}
+                />
+              ) : (
+                <div key={provider.name}>
+                  <button onClick={() => signIn(provider.id)}>
+                    Sign in with {provider.name}
+                  </button>
+                </div>
+              )
+            )}
+          </div>
+        </div>
+        <img
+          src="/assets/images/mesh-gradient.png"
+          className="absolute top-[20%] md:left-[70%] blur-2xl opacity-70 animate-pulse"
+          alt=""
+        />
       </main>
     </div>
   );
