29ba60057743b8e24380d51739ac820e31ba6beb
Noé <eon.sberk@gmail.com>
2022-11-22 21:02:33 +0100

    my uploads

--- src/pages/my-uploads.tsx
+++ src/pages/my-uploads.tsx
@@ -9,6 +9,15 @@ import { NextPageWithLayout } from "./_app";
 import { useInView } from "framer-motion";
 import { useEffect, useRef } from "react";
 import PoolStats from "../components/PoolStats";
+import {
+  HiCalendar,
+  HiChevronRight,
+  HiClock,
+  HiOutlineCalendar,
+  HiOutlineClock,
+} from "react-icons/hi";
+import Link from "next/link";
+import { format, formatDistanceToNow, isPast } from "date-fns";
 
 const MyUploads: NextPageWithLayout = () => {
   const { data: session } = useSession();
@@ -40,44 +49,62 @@ const MyUploads: NextPageWithLayout = () => {
       <h1>Hello👋, {session?.user?.name}!</h1>
       <p className="mt-2">Welcome to your personal space.</p>
       <div className="my-12">
-        <div className="card-solid">
+        {/* <div className="card-solid">
           <h3 className="text-2xl">Stats</h3>
-        </div>
+        </div> */}
       </div>
       <div>
         <div className="flex justify-between">
-          <h2>Pools</h2>
+          <h2 className="mb-2">Pools</h2>
           <span className="opacity-75">
             {(data?.pages as any[])?.[0].total}
           </span>
         </div>
-        <div className="card-solid">
-          <table>
-            <thead>
-              <tr>
-                <th>Message</th>
-                <th>Upload date</th>
-                <th>Expire date</th>
-              </tr>
-            </thead>
-            <tbody>
-              {data?.pages.map((page) =>
-                (page as any)?.pools.map((pool: any) => (
-                  <tr key={pool.id} className="w-1/4">
-                    <td>
-                      {pool.message || (
-                        <span className="opacity-30">no message</span>
-                      )}
-                    </td>
-                    <td>{new Date(pool.expiresAt).toISOString()}</td>
-                    <td>{new Date(pool.uploadTime).toISOString()}</td>
-                  </tr>
-                ))
-              )}
-            </tbody>
-          </table>
-        </div>
-        <div ref={ref}>bottom</div>
+        <ul className="card-solid p-0 divide-y">
+          {data?.pages.map((page) =>
+            (page as any)?.pools.map((pool: any) => (
+              <li key={pool.id} className="py-5 px-7 group cursor-pointer">
+                <Link href={`/files/${pool.id}`}>
+                  <a className=" flex justify-between items-center ">
+                    <div>
+                      <h4>Message</h4>
+                      <p>
+                        {pool.message || (
+                          <span className="opacity-30">no message</span>
+                        )}
+                      </p>
+                    </div>
+                    <div className="flex gap-4">
+                      <div className="flex gap-3 opacity-60">
+                        <time className="flex gap-1 items-center text-sm">
+                          <HiOutlineClock className={`text-lg`} />
+                          Expires in
+                          {" " +
+                            (pool.expiresAt
+                              ? !isPast(pool.expiresAt)
+                                ? formatDistanceToNow(pool.expiresAt)
+                                : "in 1 minute"
+                              : "never")}
+                        </time>
+                        <time className="flex gap-1 items-center text-sm">
+                          <HiOutlineCalendar className={`text-lg`} />
+                          Uploaded on
+                          {" " +
+                            format(
+                              new Date(pool.uploadTime) || new Date(),
+                              "dd.MM.yyyy"
+                            )}
+                        </time>
+                      </div>
+                      <HiChevronRight className="text-2xl group-hover:opacity-75 opacity-0 -translate-x-3 group-hover:translate-x-0 transition-all" />
+                    </div>
+                  </a>
+                </Link>
+              </li>
+            ))
+          )}
+        </ul>
+        <div ref={ref}></div>
       </div>
     </div>
   );
