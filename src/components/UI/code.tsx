5a15d31825a1cb1e63027f1e70e6d0c0c746d8d2
Noé Krebs <63413892+Sumis34@users.noreply.github.com>
2023-04-20 19:19:48 +0200

    Update code.tsx

--- src/components/UI/code.tsx
+++ src/components/UI/code.tsx
@@ -10,7 +10,7 @@ interface CodeProps {
 }
 
 const CodeEditor = dynamic(
-// eslint-disable-line
+// eslint-disable-next-line
   () => import("@uiw/react-textarea-code-editor").then((mod) => mod.default),
   { ssr: false }
 );
